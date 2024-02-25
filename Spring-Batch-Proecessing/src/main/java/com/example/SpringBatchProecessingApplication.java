ec9be30101f81823f97f185deb4406df789bb007
swapnilk30 <sk9137587676@gmail.com>
2024-02-25 14:49:12 +0530

    initial commit

---
+++ Spring-Batch-Proecessing/src/main/java/com/example/SpringBatchProecessingApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProecessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchProecessingApplication.class, args);
+	}
+
+}
